c86629e5a8906fab22bb0681684dae3adfe8e561
Chase Covington <mchasecov@gmail.com>
2021-12-17 00:22:11 -0500

    feat(tailwind): Dark/Light mode toggle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def dark_mode
+    cookies[:dark_mode] = {
+      value: 'dark mode on'
+    }
+    redirect_back(fallback_location: root_path)
+  end
+
+  def light_mode
+    cookies.delete(:dark_mode)
+    redirect_back(fallback_location: root_path)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#main'
   get 'pages/main'
+
+  get '/dark_mode', to: 'application#dark_mode', as: 'dark_mode'
+  get '/light_mode', to: 'application#light_mode', as: 'light_mode'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
