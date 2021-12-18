c33af2dbdb13c44385b3eff11dcbd06202d85cfa
Chase Covington <mchasecov@gmail.com>
2021-12-17 23:11:51 -0500

    Feat: page scroll animations
    I used the built-in listeners for turbo events and CSS events
    
    turbo:before-fetch-request allows us to pause the fetch
    (event.preventDefault())
    
    We can then append the animation_fadeOutLeft class to the outgoing region
    
    This animation automatically plays. When this animation ends (CSS event)
    we resume the loading of the uncoming page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:before-fetch-request', async function (event) {
+  event.preventDefault() // Stops the fetch of the next page
+  let region = document.getElementById("testbox") // At this point, this is still referencing the 'old' page's element.
+  region.className += " animate__fadeOutLeft" // Add the fade out CSS class to the element (which automatically fires itself)
+  region.addEventListener('animationend', function () { // Signals when the fade out animation is over.
+    event.detail.resume() // Resumes loading from our earlier preventDefault!
+  })
+})
