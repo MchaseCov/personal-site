728e016e7c96cb51f5532ba3c1dc0cd9f7682615
Chase Covington <mchasecov@gmail.com>
2021-12-18 18:59:24 -0500

    feat: animation for project show

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,32 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-document.addEventListener('turbo:before-fetch-request', async function (event) {
-  event.preventDefault() // Stops the fetch of the next page
-  let region = document.getElementById("animation-region") // At this point, this is still referencing the 'old' page's element.
-  region.className += " animate__fadeOutLeft" // Add the fade out CSS class to the element (which automatically fires itself)
-  region.addEventListener('animationend', function () { // Signals when the fade out animation is over.
-    event.detail.resume() // Resumes loading from our earlier preventDefault!
-  })
+// Drops down project preview window upon click of a project thumbnail
+document.addEventListener('turbo:before-fetch-request', async (event) => {
+  event.preventDefault()
+  // project-grid transition
+  if (event.target.id == "project-information") {
+    let region = document.getElementById("project-information")
+    if (region.classList.contains("completed")) { event.detail.resume(); return }
+    else { // Else if its not containing completed aka (!region.classList.contains("completed"))
+      region.classList.add("expand-turbo-parent");
+      region.addEventListener('animationend', () => {
+        if (!region.classList.contains("completed")) {
+          region.classList.add("completed")
+          region.classList.remove("expand-turbo-parent")
+          document.getElementById("hold").classList.add("mb-[61.7rem]") //prevents a tiny .2 second jump when the turbo frame loads in
+          event.detail.resume()
+        }
+      })
+      document.addEventListener('turbo:frame-load', async function (event) {
+        if (event.target.id == "project-information") {
+          document.getElementById("hold").classList.remove("mb-[61.7rem]")
+        }
+      })
+    }
+  } else { //side-wide transition
+    let region = document.getElementById("animation-region")
+    region.classList.add("animate__fadeOutLeft")
+    region.addEventListener('animationend', () => { event.detail.resume() })
+  }
 })
