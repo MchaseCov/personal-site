449e47ef74d732a6db195bbae3faed3bf46dea9b
Chase Covington <mchasecov@gmail.com>
2021-12-30 21:41:21 -0500

    feat: track download count of my hotwire gem

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,23 @@
 class ProjectsController < ApplicationController
+  require 'json'
+  require 'net/http'
+
   def index
     @projects = Project.all.visible.order(created_at: :desc)
   end
 
   def show
     @project = Project.find(params[:id])
+    return unless @project.title == 'Hotwire Scaffold Generator Gem'
+
+    fetch_gem_info
+  end
+
+  def fetch_gem_info
+    url = 'https://rubygems.org/api/v2/rubygems/hotwire_scaffold_generator/versions/1.0.1.json'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    result = JSON.parse(response)
+    @count = result['downloads']
   end
 end
