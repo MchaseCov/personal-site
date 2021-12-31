c2184cb99d548195d8ec95f194a23debd31b00f3
Chase Covington <mchasecov@gmail.com>
2021-12-30 21:53:39 -0500

    build: sort project by update
    allows to touch a project to bump it and customize orders

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
   require 'net/http'
 
   def index
-    @projects = Project.all.visible.order(created_at: :desc)
+    @projects = Project.all.visible.order(updated_at: :desc)
   end
 
   def show
