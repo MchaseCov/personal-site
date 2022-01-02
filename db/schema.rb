15b0aefc66087f179f80919b6ba58dcf9e8f1302
Chase Covington <mchasecov@gmail.com>
2022-01-01 19:17:01 -0500

    migrate blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_213802) do
+ActiveRecord::Schema.define(version: 2022_01_02_000114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "desc"
+    t.text "body"
+    t.string "associated_url"
+    t.string "url_text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "cards", force: :cascade do |t|
     t.string "title"
