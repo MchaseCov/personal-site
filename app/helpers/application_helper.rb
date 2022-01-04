3070e3fef1519d416a2cd44f645803d50756ec3c
Chase Covington <mchasecov@gmail.com>
2022-01-04 15:46:36 -0500

    feat: accessibility update
    Added title and description meta tags
    
    Increased contrast ratio of footer region titles
    
    Added title to anchor links with icons
    
    Added noreferrer to external links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
     options = %i[hard_wrap autolink no_intra_emphasis fenced_code_blocks]
     Markdown.new(text, *options).to_html.html_safe
   end
+
+  def title(text)
+    content_for :title, text
+  end
+
+  def meta_tag(tag, text)
+    content_for :"meta_#{tag}", text
+  end
+
+  def yield_meta_tag(tag, default_text = '')
+    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+  end
 end
