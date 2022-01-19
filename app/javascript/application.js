ec0c001b8fb3dea8546a3f301415e9313c958822
Chase Covington <mchasecov@gmail.com>
2022-01-19 10:38:31 -0500

    refactor: Split JS into more functions
     fix animation hang bug by moving listener to BEFORE animation is added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,42 +4,52 @@ import 'controllers';
 
 // TODO: Research more on Rails 7 importmaps to split this file up in a way similar to webpack
 
-// Drops down project preview window upon click of a project thumbnail
-document.addEventListener('turbo:before-fetch-request', async (event) => {
-  event.preventDefault();
-  // project-grid transition
-  if (event.target.id === 'project-information') {
-    const region = document.getElementById('project-information');
-    if (region.classList.contains('completed')) {
-      event.detail.resume();
-    } else {
-      region.classList.add('expand-turbo-parent');
-      region.addEventListener('animationend', () => {
-        if (!region.classList.contains('completed')) {
-          region.classList.add('completed');
-          region.classList.remove('expand-turbo-parent');
-          document.getElementById('hold').classList.add('mb-[61.7rem]'); // prevents a tiny .2 second jump when the turbo frame loads in
-          event.detail.resume();
-        }
-      });
-      document.addEventListener('turbo:frame-load', async (event) => {
-        if (event.target.id === 'project-information') {
-          document.getElementById('hold').classList.remove('mb-[61.7rem]');
-        }
-      });
+// ANIMATIONS ======================================================================================
+
+// PROJECT LOAD TRANSITION
+// The reason this does not simply listen for an animation end to resume, is
+// to prevent a page 'jump' in the .2 seconds where the new content is still
+// animating in, and the DOM items below the new content try to jump up and
+// fill in that space.
+
+// Prevents jump with margin
+const preventJumpWithMargin = (region, event) => {
+  region.classList.add('completed');
+  region.classList.remove('expand-turbo-parent');
+  document.getElementById('hold').classList.add('mb-[61.7rem]'); // prevents a tiny .2 second jump when the turbo frame loads in
+  event.detail.resume();
+};
+
+// Adds the grid-down animation class
+const gridDownwardAnimation = (region, event) => {
+  region.classList.add('expand-turbo-parent');
+  region.addEventListener('animationend', () => {
+    if (!region.classList.contains('completed')) {
+      preventJumpWithMargin(region, event);
     }
+  });
+};
+
+// PROJECT GRID TRANSITION Called from event listener
+const animateProjectsDown = (event) => {
+  const region = document.getElementById('project-information');
+  if (region.classList.contains('completed')) {
+    event.detail.resume();
   } else {
-    // side-wide transition
-    const region = document.getElementById('animation-region');
-    region.classList.add('animate__fadeOutLeft');
-    region.addEventListener('animationend', () => {
-      event.detail.resume();
-    });
+    gridDownwardAnimation(region, event);
   }
-});
+};
 
-// Dark Mode Toggle
+// PAGE -> PAGE SITEWIDE TRANSITION Called from event listener
+const animateNewPage = (event) => {
+  const region = document.getElementById('animation-region');
+  region.addEventListener('animationend', () => {
+    event.detail.resume();
+  });
+  region.classList.add('animate__fadeOutLeft');
+};
 
+// DARK MODE TOGGLE ================================================================================
 const html = document.querySelector('html');
 const mainButton = document.querySelector('.toggle-theme');
 const footerButton = document.querySelector('.toggle-theme-footer');
@@ -56,6 +66,8 @@ function darkMode(button) {
   button.classList.toggle('fa-moon');
 }
 
+// LISTENERS =======================================================================================
+
 // The buttons would probably be better as a click => fumction call in the HTML but I would like to
 // research if there are any pros vs cons to it first.
 mainButton.addEventListener('click', () => {
@@ -64,4 +76,21 @@ mainButton.addEventListener('click', () => {
 
 footerButton.addEventListener('click', () => {
   darkMode(footerButton);
+});
+
+// Drops down project preview window upon click of a project thumbnail
+document.addEventListener('turbo:before-fetch-request', async (event) => {
+  event.preventDefault(); // Stops BEFORE a network request is sent to fetch page
+  if (event.target.id === 'project-information') {
+    animateProjectsDown(event);
+  } else {
+    animateNewPage(event);
+  }
+});
+
+// When a project loads in, remove the margin buffer that was added to prevent jumpskip
+document.addEventListener('turbo:frame-load', async (event) => {
+  if (event.target.id === 'project-information') {
+    document.getElementById('hold').classList.remove('mb-[61.7rem]');
+  }
 });
