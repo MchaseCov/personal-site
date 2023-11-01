5430d862175e89608cb45539322394cb338f774a
Chase Covington <mchasecov@gmail.com>
2023-11-01 14:56:32 -0500

    create module for Const-saved scrollers

---
+++ app/models/concerns/card_scrollers.rb
@@ -0,0 +1,11 @@
+module CardScrollers
+  ABOUT_ME_CTA = ["Passionate", "Architecture", "Love the beach", "Ambition", "Cat lover", "Art and Design", "Curiosity Driven", "Experimentation", "Love for learning", "Innovative", "Determination", "Cooking", "South Carolina", "PC Hardware", "Momentum Workflow", "Nature", "Animal Crossing", "Figuring out \"Why?\"", "Photography", "Excited"]
+  PROJECTS_CTA = ["HTML", "Turbo", "Responsive Design", "Postgres", "MVC Frameworks", "Javascript", "RSpec", "Rails", "SQL", "CSS", "Tailwind", "Bash", "Git Push --Force", "Stimulus", "Ruby", "Data Validation", "Omniauth", "Kimurai", "Webpack" , "Vue"]
+  BLOGS_CTA    = ["Self-Teaching", "Partnership", "Documentation", "Shared Experience", "Reflection", "Open-Source", "Collaberation", "Participation", "Self-Expression", "Community", "Expertise", "Learning from Feedback", "Audience", "Learning From Mistakes", "Creative Outlet", "Journey", "Knowledge", "Technical Content", "Sharing Passions", "Guiding Others"]
+
+  SCROLLER_ASSIGNMENTS =  { 
+    "Hello & Welcome!" => ABOUT_ME_CTA,
+    "What do I do?" => PROJECTS_CTA,
+    "Get to know me!" => BLOGS_CTA
+  }
+end
